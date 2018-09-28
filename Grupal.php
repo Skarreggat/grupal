07222cc073938110abc2b4038bfa4d8788c711a9
Skarreggat <Usuario@LAPTOP-I690RMTP>
2018-09-28 16:53:05 +0200

    Class grupal

---
+++ Grupal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            
+            class grupalsito{
+                function grupal_1(){
+                    echo "Dani";
+                }
+                function grupal_2(){
+                    echo "Alex";
+                }
+                function grupal_3(){
+                    echo "Adrian";
+                }
+            }  
+        ?>
+    </body>
+</html>
