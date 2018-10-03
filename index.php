b19842097c413a7598d6b64f9c006e80dc424902
Ismael <Ismael@LAPTOP-I75TTFFK>
2018-10-03 19:27:22 +0200

    commit1

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class yeee{
+                function __construct() {
+                    
+                    echo "Yeeeee";
+                    echo "2º Yeee";
+                }
+                function metodo1(){
+                    //codigo
+                }
+                function metodo2(){
+                    //codigo2
+                }
+                function metodo3(){
+                    //codigo3
+                }
+                function fantasma(){
+                    echo "fantasmita 11";
+                }
+            }
+            
+            class grupalsito{
+                function grupal_1(){
+                    echo "Dani";
+                }
+                function grupal_2(){
+                    echo "Alex";
+                }
+                function grupal_3(){
+                    echo "Adrian";
+                }
+            }
+        ?>
+    </body>
+</html>
---
+++ ismael.php
@@ -0,0 +1,24 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class ismael{
+    
+    function alex(){
+        
+    }
+    
+    function dani(){
+        
+    }
+    
+    function ismael(){
+        
+    }
+}
+?>
+}
